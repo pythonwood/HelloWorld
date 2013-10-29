6cf8c6cdef7a5e5a34a97717eb10db0ad43d4d4a
pythonwood <582223837@qq.com>
2013-10-29 23:50:01 +0800

    in master the same branch for merge

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,3 +6,4 @@ import os,sys,time
 print(time.ctime()+" Hello World !")
 print('\n'.join(sys.path))
 print("master merge test after rebase test is esaier")
+print("the same branch for merge for test 2")
